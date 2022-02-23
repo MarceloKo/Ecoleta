c612917f3312e40f7a44c10a7573bb0e92689ba8
MarceloKo <marceloakohlhase@gmail.com>
2022-02-23 18:00:31 -0400

    Back-end finalizado

--- server/src/database/migrations/02_create_point_items.ts
+++ server/src/database/migrations/02_create_point_items.ts
@@ -1,2 +1,23 @@
-export async function up(){}
-export async function down(){}
+import {Knex} from "knex";
+
+export async function up(knex: Knex){
+    //CRIAR  A TABELA
+    return knex.schema.createTable('point_items',table=>{
+        table.increments('id').primary()
+
+        table.string('point_id')
+            .notNullable()
+            .references('id')
+            .inTable('points');
+
+        table.string('item_id')
+            .notNullable()
+            .references('id')
+            .inTable('items');
+    })
+
+}
+export async function down(knex:Knex){
+    //VOLTAR ATRAS(DELETAR A TABELA)
+    return knex.schema.dropTable('point_items');
+}
