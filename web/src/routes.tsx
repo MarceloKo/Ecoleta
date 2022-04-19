05c101002693fd53fdc25ebffb6e30d43ecaca8e
MarceloKo <marceloakohlhase@gmail.com>
2022-04-19 18:40:27 -0400

    Tela inicial e criando as rotas

---
+++ web/src/routes.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {Route,BrowserRouter,Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import { CreatePoint } from "./pages/CreatePoint";
+
+
+const Routess = () => {
+    return (
+      <BrowserRouter>
+       <Routes>
+          <Route path="/" element={<Home/>}/>   
+          <Route path="/create-point" element={<CreatePoint/>}/>   
+       </Routes>
+      </BrowserRouter>
+      
+    )
+        
+      
+}
+
+export default Routess;
